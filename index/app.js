36733e6d5c2e6e7040545bf30d7b5083765a1d1a
Alexanderrrr <alexanderfavvas@gmail.com>
2025-08-20 18:19:01 -0700

    fixed a thing

--- index/app.js
+++
@@ -1,7 +0,0 @@
-const navToggle=document.getElementById('navToggle');
-const primaryNav=document.getElementById('primaryNav');
-if(navToggle&&primaryNav){
-  navToggle.addEventListener('click',()=>{
-    primaryNav.classList.toggle('open');
-  });
-}
